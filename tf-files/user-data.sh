a26cc392215234b03fc88d00d1f7b6d1af8c7665
Yunus Altay <yunusaltay.nl@gmail.com>
2023-09-13 14:42:25 +0200

    Userdata editted

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -11,6 +11,6 @@ chmod +x /usr/local/bin/docker-compose
 dnf install git -y
 cd /home/ec2-user && git clone https://github.com/Yunus-Altay/202-dockerization-bookstore-api-on-python-flask-mysql.git
 cd /home/ec2-user/202-dockerization-bookstore-api-on-python-flask-mysql
-echo ${db_password} > db_password.txt
-echo ${db_root_password} > db_root_password.txt
+echo ${db_password} > db_password.txt # do not forget to adjust the python file
+echo ${db_root_password} > db_root_password.txt # # do not forget to adjust the python file
 docker-compose up -d
